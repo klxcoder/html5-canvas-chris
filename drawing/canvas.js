d122674ed2116c84bad6becabce858f26b2f8595
klxcoder <klxcoder@gmail.com>
2025-03-26 16:16:24 +0700

    draw line

--- drawing/canvas.js
+++ drawing/canvas.js
@@ -2,4 +2,14 @@ const canvas = document.querySelector('canvas')
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const c = canvas.getContext('2d')
+
+//
 c.fillRect(100, 100, 100, 100)
+c.fillRect(400, 100, 100, 100)
+c.fillRect(300, 300, 100, 100)
+
+// Line
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.stroke();
