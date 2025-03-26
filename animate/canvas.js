0ae3ccad79545344eb68b3839c0c8cb073fc0cdc
klxcoder <klxcoder@gmail.com>
2025-03-26 16:32:32 +0700

    comment

--- animate/canvas.js
+++ animate/canvas.js
@@ -4,20 +4,20 @@ canvas.height = window.innerHeight;
 const c = canvas.getContext('2d')
 
 //
-c.fillStyle = 'rgba(255, 0, 0, 0.5)'
-c.fillRect(100, 100, 100, 100)
-c.fillStyle = 'rgba(0, 0, 255, 0.5)'
-c.fillRect(400, 100, 100, 100)
-c.fillStyle = 'rgba(0, 255, 0, 0.5)'
-c.fillRect(300, 300, 100, 100)
+// c.fillStyle = 'rgba(255, 0, 0, 0.5)'
+// c.fillRect(100, 100, 100, 100)
+// c.fillStyle = 'rgba(0, 0, 255, 0.5)'
+// c.fillRect(400, 100, 100, 100)
+// c.fillStyle = 'rgba(0, 255, 0, 0.5)'
+// c.fillRect(300, 300, 100, 100)
 
-// Line
-c.beginPath();
-c.moveTo(50, 300);
-c.lineTo(300, 100);
-c.lineTo(400, 300);
-c.strokeStyle = '#fa34a3';
-c.stroke();
+// // Line
+// c.beginPath();
+// c.moveTo(50, 300);
+// c.lineTo(300, 100);
+// c.lineTo(400, 300);
+// c.strokeStyle = '#fa34a3';
+// c.stroke();
 
 // Arc / Circle
 // c.beginPath();
@@ -25,11 +25,11 @@ c.stroke();
 // c.strokeStyle = 'blue'
 // c.stroke();
 
-for (let i = 0; i < 3; i++) {
-  const x = Math.random() * window.innerWidth;
-  const y = Math.random() * window.innerHeight;
-  c.beginPath();
-  c.arc(x, y, 30, 0, Math.PI * 2, false)
-  c.strokeStyle = 'blue'
-  c.stroke();
-}
+// for (let i = 0; i < 3; i++) {
+//   const x = Math.random() * window.innerWidth;
+//   const y = Math.random() * window.innerHeight;
+//   c.beginPath();
+//   c.arc(x, y, 30, 0, Math.PI * 2, false)
+//   c.strokeStyle = 'blue'
+//   c.stroke();
+// }
