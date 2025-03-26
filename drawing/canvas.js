7c0611e595ee2f88702e5a92dfcdcea6e3434516
klxcoder <klxcoder@gmail.com>
2025-03-26 16:20:18 +0700

    fillStyle

--- drawing/canvas.js
+++ drawing/canvas.js
@@ -8,6 +8,7 @@ c.fillStyle = 'rgba(255, 0, 0, 0.5)'
 c.fillRect(100, 100, 100, 100)
 c.fillStyle = 'rgba(0, 0, 255, 0.5)'
 c.fillRect(400, 100, 100, 100)
+c.fillStyle = 'rgba(0, 255, 0, 0.5)'
 c.fillRect(300, 300, 100, 100)
 
 // Line
